ba7fe71eba0f0c23b19a653eafe3c786e70b9eb2
Samuel Hoffstaetter <sam@shortstorybox.com>
2024-01-23 15:38:59 -0800

    Re-implement using a more reliable subsystem. (#2)
    The new implementation is much cleaner and avoids the previous issues
    with card double-scans.

--- src/smartcard_identifier.py
+++ src/smartcard_identifier.py
@@ -1,48 +1,45 @@
 #!/usr/bin/env python3
+import shutil
+import argparse
 import platform
 import sys
 import time
 from binascii import hexlify
 from subprocess import PIPE, Popen
+import getpass
 
-import smartcard.util
-from smartcard.scard import (INFINITE, SCARD_E_TIMEOUT, SCARD_PROTOCOL_T0,
-                             SCARD_PROTOCOL_T1, SCARD_S_SUCCESS,
-                             SCARD_SCOPE_USER, SCARD_SHARE_SHARED,
-                             SCARD_STATE_PRESENT, SCARD_STATE_UNAWARE,
-                             SCARD_UNPOWER_CARD, SCardConnect, SCardDisconnect,
-                             SCardEstablishContext, SCardGetErrorMessage,
-                             SCardGetStatusChange, SCardListReaders,
-                             SCardReleaseContext, SCardTransmit)
+from smartcard.CardRequest import CardRequest
+from smartcard.Exceptions import CardConnectionException, NoCardException
 
-TIMEOUT = 15  # seconds
-SYSTEM = None
-
-
+_system = None
 def platform_system():
-    global SYSTEM
-    if not SYSTEM:
-        SYSTEM = platform.system()
-    return SYSTEM
+    global _system
+    if not _system:
+        _system = platform.system()
+    return _system
 
 
-def simulate_keypress(text):
+_last_simulated_keypress = 0
+def simulate_keypress(text, use_linux_uinput):
     """
     Paste the given text into the current application.
     """
     if not text.replace("\n", "").replace(" ", "").isalnum():
         raise Exception(
-            "Keystroke text must be alphanumeric with spaces/newlines allowed"
+            "Keystroke text must be alphanumeric (spaces/newlines allowed)"
         )
 
     if platform_system() == "Darwin":
         cmd = [
             "osascript",
             "-e",
-            f'tell application "System Events" to keystroke "{text}"',
+            f'tell application "System Events" to keystroke "{card_id}"',
         ]
     elif platform_system() == "Linux":
-        cmd = ["xdotool", "type", "--delay", "0", text]
+        if use_linux_uinput:
+            cmd = ["ydotool", "type", "--next-delay", "0", "--key-delay", "0", text]
+        else:
+            cmd = ["xdotool", "type", "--delay", "0", text]
     elif platform_system() == "Windows":
         cmd = [
             "powershell",
@@ -52,94 +49,88 @@ def simulate_keypress(text):
     else:
         raise Exception(f"Unsupported platform: {platform.system()}")
 
-    p = Popen(cmd, stdout=PIPE, stderr=PIPE)
+    global _last_simulated_keypress
+    if _last_simulated_keypress > time.time() - 0.1:
+         # This helps reduce certain race conditions on certain platforms
+         # when keypresses are simulated too quickly.
+        time.sleep(0.1)
+        _last_simulated_keypress = time.time()
+
+    try:
+        p = Popen(cmd, stdout=PIPE, stderr=PIPE)
+    except FileNotFoundError:
+        sys.stderr.write(f'ERROR: Could not find command "{cmd[0]}". Please install it on your system.\n')
+        return
     stdout, stderr = p.communicate()
     if p.returncode != 0:
-        print(f'Failed to simulate keypress: {stderr.decode("utf-8")}')
+        sys.stderr('Failed to simulate keypress: ')
+        sys.stderr.write(stderr)
+        sys.stderr.write('\n')
+        sys.stderr.flush()
 
-
-def process_card(context, reader):
-    """
-    Process a card that was inserted into the reader.
-    """
-    result, card, protocol = SCardConnect(
-        context, reader, SCARD_SHARE_SHARED, SCARD_PROTOCOL_T0 | SCARD_PROTOCOL_T1
-    )
-    if result != SCARD_S_SUCCESS:
-        print(f"Failed to connect to card: {SCardGetErrorMessage(result)}")
-        return
-    try:
-        QUERY_CARD_ID = [0xFF, 0xCA, 0x00, 0x00, 0x00]
-        result, response = SCardTransmit(card, protocol, QUERY_CARD_ID)
-        if result != SCARD_S_SUCCESS:
-            print(f"Failed to read card ID: {SCardGetErrorMessage(result)}")
-            return
-        card_id = hexlify(bytes(response)).decode("ascii").upper()[:14]
-        simulate_keypress(card_id + "\n")
-    finally:
-        result = SCardDisconnect(card, SCARD_UNPOWER_CARD)
-        if result != SCARD_S_SUCCESS:
-            print("Failed to disconnect: {SCardGetErrorMessage(result)}")
-
-
-def run(context):
-    """
-    Run the main loop that waits for a card reader to be connected to the
-    computer, and for a card to be inserted into the reader.
-    """
-
-    readers, readerstates = [], []
+def run(print_only, use_linux_uinput):
+    cardrequest = CardRequest(timeout=None, newcardonly=True)
     while True:
-        previous_readers = readers
-        while True:
-            result, readers = SCardListReaders(context, [])
-            if result != SCARD_S_SUCCESS:
-                sys.exit(
-                    f"Failed to determine if there are NFC readers: {SCardGetErrorMessage(result)}"
-                )
-            if readers:
-                break
+        card = cardrequest.waitforcard()
+        try:
+            try:
+                card.connection.connect()
+            except NoCardException as err:
+                sys.stderr.write(f'{err}\n')
+                sys.stderr.flush()
+                continue
+
+            QUERY_CARD_ID = [0xFF, 0xCA, 0x00, 0x00, 0x00]
+            try:
+                response, sw1, sw2 = card.connection.transmit(QUERY_CARD_ID)
+            except CardConnectionException as err:
+                sys.stderr.write(f'{err}\n')
+                sys.stderr.flush()
+                continue
+
+            SUCCESS = (0x90, 0x00)
+            if (sw1, sw2) == SUCCESS:
+                card_id = hexlify(bytes(response)).decode("ascii").upper()
+                if print_only:
+                    print(card_id)
+                else:
+                    simulate_keypress(card_id, use_linux_uinput=use_linux_uinput)
             else:
-                time.sleep(TIMEOUT)
+                sys.stderr.write('Failed to read card ID\n')
+                sys.stderr.flush()
+        finally:
+            card.connection.disconnect()
 
-        if readers != previous_readers:
-            # Re-initialize readerstates to account for new and/or removed readers
-            result, readerstates = SCardGetStatusChange(
-                context, 0, [(r, SCARD_STATE_UNAWARE) for r in readers]
-            )
-            if result != SCARD_S_SUCCESS:
-                sys.exit(
-                    f"Failed to get NFC reader status: {SCardGetErrorMessage(result)}"
-                )
+def main():
+    parser = argparse.ArgumentParser(description="Wait for a smart card to be scanned, then emulate a keyboard to paste the card ID as text.")
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("--x11", action="store_true", help="Use X11 to simulate keypresses. (Linux only)")
+    group.add_argument("--uinput", action="store_true", help="Use uinput to simulate keypresses. Requires user to be in the 'input' group, or to run as root. (Linux only)")
+    group.add_argument("--print", action="store_true", help="Print the card ID to stdout instead of simulating keypresses.")
+    args = parser.parse_args()
+    if platform_system() == "Linux":
+        if args.uinput:
+            shutil.which("ydotool") or sys.exit("Please install `ydotool`, e.g.:\n\n    $ sudo apt-get install ydotool")
+        elif args.x11:
+            shutil.which("xdotool") or sys.exit("Please install `xdotool`, e.g.:\n\n    $ sudo apt-get install xdotool")
+        elif args.print:
+            pass
+        else:
+            parser.print_help()
+            sys.exit("ERROR: You must specify either --x11 or --uinput or --print")
 
-        while True:
-            if not readerstates:
-                raise AssertionError(
-                    "readerstates is empty, which should be impossible"
-                )
-            result, readerstates = SCardGetStatusChange(
-                context, TIMEOUT * 1000, readerstates
-            )
-            if result == SCARD_S_SUCCESS:
-                for reader, eventstate, atr in readerstates:
-                    if eventstate & SCARD_STATE_PRESENT:
-                        process_card(context, reader)
-            elif result == SCARD_E_TIMEOUT:
-                break  # restart loop to check if a new reader was connected to the computer
-            else:
-                sys.exit(
-                    f"Failed to get NFC reader status: {SCardGetErrorMessage(result)}"
-                )
-
+    do_run = lambda *_, **__: run(print_only=args.print, use_linux_uinput=args.uinput)
+    if args.print:
+        # Disable keyboard echoing on the terminal, which can cause confusion
+        # if the script is running at the same time somewhere else
+        _raw_input = getpass._raw_input
+        try:
+            getpass._raw_input = do_run
+            getpass.getpass("")
+        finally:
+            getpass._raw_input = _raw_input
+    else:
+        do_run()
 
 if __name__ == "__main__":
-    result, context = SCardEstablishContext(SCARD_SCOPE_USER)
-    if result != SCARD_S_SUCCESS:
-        sys.exit(f"Failed to establish NFC context: {SCardGetErrorMessage(result)}")
-
-    try:
-        run(context)
-    finally:
-        result = SCardReleaseContext(context)
-        if result != SCARD_S_SUCCESS:
-            sys.exit(f"Failed to release NFC context: {SCardGetErrorMessage(result)}")
+    main()
